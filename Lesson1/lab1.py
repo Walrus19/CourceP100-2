3bea54c082ec12bdb099ad854bf1403992ea4446
Walrus19 <63751475+Walrus19@users.noreply.github.com>
2024-10-18 20:05:51 +0300

    Delete Lesson1 directory

--- Lesson1/lab1.py
+++
@@ -1,8 +0,0 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-
-# TODO заменить значение пропущенного элемента средним арифметическим
-average = round((sum(numbers[0:numbers.index(None)])+sum(numbers[numbers.index(None)+1:])) / len(numbers), 2)
-a = numbers.index(None)
-
-numbers[4] = average
-print("Измененный список:", numbers)
